80e99144a783f2602b34fa48bb6353d671c61e53
dangtoi05122003 <dangtoi05122003@gmail.com>
2026-06-28 16:31:59 +0700

    fix(#1): change JWT auth from JWK to HS512 secret key

--- api-gateway/src/main/java/com/minibank/api_gateway/config/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/minibank/api_gateway/config/SecurityConfiguration.java
@@ -1,33 +1,45 @@
 package com.minibank.api_gateway.config;
 
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    private String jwkUri;
-
+    @Value("${jwt.signerkey}")
+    private String secret;
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
             .csrf(ServerHttpSecurity.CsrfSpec::disable)
-            .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-            .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
             .authorizeExchange(exchanges -> exchanges
-                .pathMatchers("/user/register").permitAll()
+                .pathMatchers("/user/register", "/auth/**").permitAll()
                 .anyExchange().authenticated()
             )
             .oauth2ResourceServer(oAuth2ResourceServer -> 
-                oAuth2ResourceServer.jwt(jwt -> jwt.jwkSetUri(jwkUri))
+                oAuth2ResourceServer.jwt(jwt -> jwt.jwtDecoder(jwtDecoder()))
             )
             
             .build();
+    }
+    @Bean
+    ReactiveJwtDecoder jwtDecoder() {
+        SecretKeySpec key =
+                new SecretKeySpec(secret.getBytes(), "HmacSHA512");
+
+        return NimbusReactiveJwtDecoder
+                .withSecretKey(key)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
     }
 }
